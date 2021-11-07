162f35e148266b394a7dfd0df6e5c8bb8afaa5e0
Pratham <62070812+prathami1@users.noreply.github.com>
2021-11-07 18:20:31 -0500

    Star Wars commit (finished)

--- StarWars/StarWars.java
+++ StarWars/StarWars.java
@@ -6,6 +6,10 @@ public class StarWars
     public StarWars()
     {
         File name = new File("StarWars/input.csv");
+        Stack<Character> male = new Stack<Character>();   
+        Stack<Character> female = new Stack<Character>();   
+        Stack<Character> droids = new Stack<Character>();   
+        Stack<Character> birthYear = new Stack<Character>();  
 		try
 		{
 			BufferedReader input = new BufferedReader(new FileReader(name));
@@ -13,78 +17,139 @@ public class StarWars
 			String text, output = "";
 			while( (text=input.readLine())!= null)
 			{
-				String[] pieces = text.split("\n"); 
-				output += text;
-                for(int i = 0; i < pieces.length; i++)
-                {
-                    sort(pieces[i]);
-                }
-			}
+                sort(text, male, female, droids, birthYear);
+            }
 		}
 		catch (IOException io)
 		{
 			System.err.println("File does not exist");
 		}
+
+        System.out.println("Male Characters");
+        System.out.println(String.format("%s%50s", "Name", "HomeWorld"));
+        while (!male.empty()) 
+        {
+            System.out.println(male.pop());
+        }
+
+        System.out.println("\nFemale Characters");
+        System.out.println(String.format("%s%50s", "Name", "HomeWorld"));
+        while (!female.empty()) 
+        {
+            System.out.println(female.pop());
+        }
+
+        System.out.println("\nDroids");
+        System.out.println(String.format("%s%50s", "Name", "HomeWorld"));
+        while (!droids.empty()) 
+        {
+            System.out.println(droids.pop());
+        }
+
+        System.out.println("\nAges");
+        System.out.println(String.format("%s%50s%25s", "Name", "HomeWorld", "Birth Year (BBY)"));
+        while(!birthYear.empty()) 
+        {
+            Character character = birthYear.pop();
+            if(character.homeWorld.equals("NA"))
+            {
+                if(character.birthYear.contains("."))
+                {
+                    System.out.println(String.format("%-45s%-18s%s", character.name, character.homeWorld, character.birthYear));
+                }
+                else
+                {
+                    System.out.println(String.format("%-45s%-18s%s", character.name, character.homeWorld, character.birthYear + ".0"));
+                }
+            }
+            else
+            {
+                if(character.birthYear.contains("."))
+                {
+                    System.out.println(String.format("%-45s%-18s%s", character.name, "Unknown", character.birthYear));
+                }
+                else
+                {
+                    System.out.println(String.format("%-45s%-18s%s", character.name, "Unknown", character.birthYear + ".0"));
+                }
+            }
+        }
     }
     public static void main(String[] args)
     {
         StarWars app = new StarWars();
     }
 
-    public static void sort(String input)
-    {
-        Stack<String> male = new Stack<String>();   
-        Stack<String> female = new Stack<String>();   
-        Stack<String> droids = new Stack<String>();   
-        Stack<String> birthYear = new Stack<String>();   
+    public static void sort(String input, Stack male, Stack female, Stack droids, Stack birthYear)
+    {   
         String[] pieces = input.split(",");
-		if(pieces[6].equals("male"))
+        if(pieces.length == 9)
         {
-            for(int i = 0; i < pieces.length; i++)
+            if(pieces[6].equals("male"))
             {
-                male.push(pieces[i]);
+                male.push(new Character(pieces[0], pieces[5], pieces[6], pieces[7], pieces[8]));
+            }
+            else if(pieces[6].equals("female"))
+            {
+                female.push(new Character(pieces[0], pieces[5], pieces[6], pieces[7], pieces[8]));
+            }
+            if(pieces[8].equals("Droid"))
+            {
+                droids.push(new Character(pieces[0], pieces[5], pieces[6], pieces[7], pieces[8]));
+            }
+            if(pieces[5].equals("NA"))
+            {
+                //birthYear.push(new Character(pieces[0], pieces[5].substring(0, pieces[5].length() - 3), pieces[6], pieces[7], pieces[8]));
+                birthYear.push(new Character(pieces[0], pieces[5], pieces[6], pieces[7], pieces[8]));
             }
         }
-        else if(pieces[6].equals("female"))
+        else if(pieces.length == 10)
         {
-            for(int i = 0; i < pieces.length; i++)
+            if(pieces[7].equals("male"))
             {
-                female.push(pieces[i]);
+                male.push(new Character(pieces[0], pieces[6], pieces[7], pieces[8], pieces[9]));
+            }
+            else if(pieces[7].equals("female"))
+            {
+                female.push(new Character(pieces[0], pieces[6], pieces[7], pieces[8], pieces[9]));
+            }
+            if(pieces[8].equals("Droid"))
+            {
+                droids.push(new Character(pieces[0], pieces[6], pieces[7], pieces[8], pieces[9]));
+            }
+            if(pieces[6].equals("NA"))
+            {
+                //birthYear.push(new Character(pieces[0], pieces[6].substring(0, pieces[6].length() - 3), pieces[7], pieces[8], pieces[9]));
+                birthYear.push(new Character(pieces[0], pieces[6], pieces[7], pieces[8], pieces[9]));
             }
         }
+
     }    
+}
 
-    public class Character
+class Character 
+{
+    public String name;
+    public String birthYear;
+    public String gender;
+    public String homeWorld;
+    public String species;
+
+    public Character(String name, String birthYear, String gender, String homeWorld, String species) 
     {
-        String name;
-        int birthYear;
-        String gender;
-        String homeWorld;
-        String species;
+        this.name = name;
+        this.birthYear = birthYear;
+        this.gender = gender;
+        this.homeWorld = homeWorld;
+        this.species = species;
+    }
 
-        public String getName()
+    public String toString() 
+    {
+        if (!homeWorld.equals("NA")) 
         {
-            return name;
+            return String.format("%-45s%-5s", name, homeWorld);
         }
-
-        public int getBirthYear()
-        {
-            return birthYear;
-        }
-
-        public String getGender()
-        {
-            return gender;
-        }
-
-        public String getHomeWorld()
-        {
-            return homeWorld;
-        }
-
-        public String species()
-        {
-            return species;
-        }
+        return String.format("%-45s%-5s", name, "Unknown");
     }
 }
