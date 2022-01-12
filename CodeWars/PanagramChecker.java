4320d1e0bdce34c866125bda9b22d3d604cecb88
Pratham <10015405@sbstudents.org>
2022-01-11 23:07:57 -0500

    Delete PanagramChecker.java

--- CodeWars/PanagramChecker.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class PangramChecker {
-  public boolean check(String sentence){
-    //code
-    String replaced = sentence.replaceAll("[^A-Za-z]", "");
-    Set<String> letters = new HashSet<>(Arrays.asList(replaced.split("")));
-    return letters.size() >= 26;
-  }
-}
