e213a28ffc2b16cb5d56e3f8f2e49b6b2674e9d3
Pratham <10015405@sbstudents.org>
2022-01-26 08:03:02 -0500

    Update DoubleLinear.java

--- CodeWars/TwiceLinear/DoubleLinear.java
+++ CodeWars/TwiceLinear/DoubleLinear.java
@@ -1,28 +1,17 @@
 import java.util.*;
-
 class DoubleLinear {
     
-    public static int dblLinear (int n) {
-        Integer x = 1;
-        int y = 0, z = 0, counter = 0, listSize = 3, maxtimes = n+(n/2);
-        TreeSet<Integer> list = new TreeSet<>();
-        list.add(1);
-        while(listSize < maxtimes)
-        {
-            y = 2*x+1;
-            z = 3*x+1;
-            if (list.add(y))
-            {
-                listSize++;
-            }
-            if (list.add(z))
-            {
-                listSize++;
-            }
-            counter++;
-            x = new ArrayList<>(list).get(counter);
-        }
-        x = new ArrayList<>(list).get(n);
-        return x;
-    }
+  public static int dblLinear (int n) {
+      if(n==0) 
+        return 1;
+      TreeSet<Integer> set = new TreeSet<>();
+      set.add(1);
+      for (int i = 0; i < n; i++){
+        int x = set.first();
+        set.add(x*2+1);
+        set.add(x*3+1);
+        set.remove(x);
+      }
+      return set.first();
+  }
 }
